b751889f4534920a566adb9c726b64fb27056546
frottier <frottier@users.noreply.github.com>
2023-02-22 21:31:53 +0100

    wip: typo_replace

--- skripte/typo_replace.py
+++ skripte/typo_replace.py
@@ -25,9 +25,9 @@ def show_match_context(txt_stream, start, end):
     pos_min = 0
     con_start = max(pos_min, start - context_length)
     con_end = min(pos_max, end + context_length)
-    print(f'\n{txt_stream[con_start:start]}'
+    print(f'{txt_stream[con_start:start]}'
           f'\033[01m{txt_stream[start:end]}\033[0m'
-          f'{txt_stream[end:con_end]}\n')
+          f'{txt_stream[end:con_end]}')
 
 
 # read the text file
@@ -40,9 +40,17 @@ for match_case in replacements:
 
     for occurance in re.finditer(pattern, text):
         start, end = occurance.span()
+        the_match = occurance.group(0)
+        if match_case == 'HYPHEN-MINUS':
+            sub_text = text[:start] + the_match.replace('-', '–') + text[start+3:]
+        elif match_case == 'QUOTATION MARK open':
+            sub_text = text[:start] + the_match.replace('"', '„') + text[start+2:]
+        elif match_case == 'QUOTATION MARK close':
+            sub_text = text[:start] + the_match.replace('"', '“') + text[start+2:]
         show_match_context(text, start, end)
+        show_match_context(sub_text, start, end)
         
-        input()
+        input('? j/n')
 
 
 print(text)
